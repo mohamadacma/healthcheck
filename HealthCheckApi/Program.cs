a6e16a417936c4cdf73d3d4d77a58a898807279d
Mohamad <mohamadacma@gmail.com>
2025-07-30 22:27:49 -0500

    Delete weatherforecast endpoint

--- HealthCheckApi/Program.cs
+++ HealthCheckApi/Program.cs
@@ -179,25 +179,6 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapGet("/ping", () => "pong");
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 
 // GET to retireve item by ID
@@ -617,10 +598,7 @@ app.MapHealthChecks("/health/live");
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
 
 public partial class Program { }
 
